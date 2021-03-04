95e181d919efa34e1d746620b55d1d01bc3f9891
guoxiang2 <guoxiang2@yy.com>
2021-03-04 11:22:33 +0800

    log_writer rewrite

--- db/log_writer.cc
+++ db/log_writer.cc
@@ -2,111 +2,85 @@
 // Created by kuiper on 2021/2/18.
 //
 
-#include "db/log_writer.h"
 #include "leveldb/env.h"
+#include "db/log_writer.h"
 #include "port/port.h"
 #include "util/coding.h"
 #include "util/crc32c.h"
 
-namespace leveldb::log {
+namespace leveldb {
+namespace log {
 
-    /**
-     * @brief 
-     * @param type_crc 
-    */
-    static void InitTypeCrc(uint32_t *type_crc) {
-        for (int i = 0; i <= kMaxRecordType; i++) {
-            char t = static_cast<char>(i);
-            type_crc[i] = crc32c::Value(&t, 1);
+        /**
+         * @brief
+         * @param type_crc
+        */
+        static void InitTypeCrc(uint32_t* type_crc) 
+        {
+            for (int i = 0; i <= kMaxRecordType; i++) {
+                char t = static_cast<char>(i);
+                type_crc[i] = crc32c::Value(&t, 1);
+            }
         }
-    }
 
-    Writer::Writer(WritableFile *dest) : dest_(dest), block_offset_(0) {
-        InitTypeCrc(type_crc_);
-    }
+        Writer::Writer(WritableFile* dest) : dest_(dest), block_offset_(0)
+        {
+            InitTypeCrc(type_crc_);
+        }  
 
-    Writer::Writer(WritableFile *dest, uint64_t dest_length)
-            : dest_(dest), 
-              block_offset_(dest_length % kBlockSize) {
-        InitTypeCrc(type_crc_);
-    }
+        Writer::Writer(WritableFile* dest, uint64_t dest_length)
+            : dest_(dest), block_offset_(dest_length% kBlockSize)
+        {
+            InitTypeCrc(type_crc_);
+        }
 
-    Status Writer::AddRecord(const Slice &slice) {
-        const char *ptr = slice.data();
-        size_t left = slice.size();
+        Status Writer::AddRecord(const Slice& slice)
+        {
+           size_t size = slice.size();
 
-        Status s;
-        bool isBegin = true;
-        do {
-            // leftover : 当前块剩余可写入数.
-            const int leftover = kBlockSize - block_offset_;
-            assert(leftover >= 0);
+        }
 
-            // 如果当前block不足于写入一个Header,则全部补满0
-            if (leftover < kHeaderSize) {
-                if (leftover > 0) {
-                    static_assert(kHeaderSize == 7, "kHeaderSize of log file <> 7");
-                    // <7 最多6个
-                    dest_->Append(Slice("\x00\x00\x00\x00\x00\x00", leftover));
-                }
-                block_offset_ = 0;
-            }
+        /**
+         * ||                        header                              ||payload||
+         * ||crc32_1||crc32_2||crc32_3||crc32_4||length_1||length_2||type||payload||
+         * @brief 
+         * @param type 
+         * @param ptr 
+         * @param length 
+         * @return 
+        */
+        Status Writer::EmitPhysicalRecord(RecordType type, const char* ptr, size_t length) {
 
-            assert(kBlockSize - block_offset_ - kHeaderSize >= 0);
+            // 每个物理块的长度用2字节存储，所以只能最大0xffff
+            assert(length <= 0xffff);
+            
+            // 确保当前block可以写下当前物理块
+            assert(static_cast<size_t>(block_offset_) + static_cast<size_t>(kHeaderSize) + length < kBlockSize);
 
-            // avail: 当前块扣除header后还可以写入的空间.
-            // avail和fragment_len都可能为0
-            const size_t avail = kBlockSize - block_offset_ - kHeaderSize;
-            const size_t fragment_len = left < avail ? left : avail;
+            char buf[kHeaderSize];
+            buf[4] = static_cast<char>(length & 0xff);
+            buf[5] = static_cast<char>(length >> 8ull);
+            buf[6] = static_cast<char>(type);
 
-            RecordType type;
-            const bool isEnd = (left == fragment_len);
+            // 计算并写入CRC32C
+            uint32_t crc = crc32c::Extend(type_crc_[type], ptr, length);
+            crc = crc32c::Mask(crc);
+            EncodeFixed32(buf, crc);
 
-            if (isBegin && isEnd) {
-                type = kFullType;
-            } else if (isBegin) {
-                type = kFirstType;
-            } else if (isEnd) {
-                type = kLastType;
-            } else {
-                type = kMiddleType;
-            }
-
-            s = EmitPhysicalRecord(type, ptr, fragment_len);
-            ptr += fragment_len;
-            left -= fragment_len;
-            isBegin = false;
-
-        } while (s.IsOK() && left > 0);
-        return s;
-    }
-
-    Status Writer::EmitPhysicalRecord(RecordType type, const char *ptr, size_t length) {
-        assert(length <= 0xffff);  // 2 bytes max
-        assert(block_offset_ + kHeaderSize + length <= kBlockSize);
-
-        char buf[kHeaderSize];
-        buf[4] = static_cast<char>(length & 0xffull);
-        buf[5] = static_cast<char>(length >> 8ull);
-        buf[6] = static_cast<char>(type);
-
-        // 计算并写入CRC32C
-        uint32_t crc = crc32c::Extend(type_crc_[type], ptr, length);
-        crc = crc32c::Mask(crc);
-        EncodeFixed32(buf, crc);
-
-        // 写入物理块
-        Status s = dest_->Append(Slice(buf, kHeaderSize));
-        if (LIKELY(s.IsOK())) {
-            s = dest_->Append(Slice(ptr, length));
+            // 写入物理块
+            Status s = dest_->Append(Slice(buf, kHeaderSize));
             if (LIKELY(s.IsOK())) {
-                s = dest_->Flush();
+                s = dest_->Append(Slice(ptr, length));
+                if (LIKELY(s.IsOK())) {
+                    s = dest_->Flush();
+                }
             }
+
+            // 无论写入是否成功，offset都跳过这个区域.
+            block_offset_ += static_cast<int>((kHeaderSize + length));
+            return s;
         }
 
-        // 如果写入s.IsNotOK(),也直接累计offset跳过这个段
-        block_offset_ += static_cast<int>((kHeaderSize + length));
-        return s;
-    }
 
-}
+} // end of namespace log  
+} // end of namespace leveldb
